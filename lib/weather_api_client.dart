f16c3cb5b9adb26bf922537900e630a852af42f6
arishahmad661 <99143468+arishahmad661@users.noreply.github.com>
2023-08-08 19:24:22 +0530

    Update weather_api_client.dart

--- lib/weather_api_client.dart
+++ lib/weather_api_client.dart
@@ -7,7 +7,7 @@ import 'models/weather.dart';
 
 class WeatherApiClient{
   Future getCurrentWeather(String? location) async{
-    var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=de8100c638c6f22f05556317c3cef59c&units=metric&units=metric");
+    var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=<PUT_YOUR_API_KEY_HERE>&units=metric&units=metric");
     var response = await http.get(url);
     var body = jsonDecode(response.body);
     return Weather.fronJson(body);
